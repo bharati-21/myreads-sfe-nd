4cc1653e268c0ade17f31d680fe7fe7009ce94b1
Bharati Subramanian <bharatisharada@gmail.com>
2022-11-28 17:03:55 +0530

    feat: app constants

---
+++ starter/src/Constants.js
@@ -0,0 +1,7 @@
+export const constants = {
+	SHELVES: ["wantToRead", "currentlyReading", "read"],
+	currentlyReading: "Currently Reading",
+	wantToRead: "Want to Read",
+	read: "Read",
+    none: "None"
+};
