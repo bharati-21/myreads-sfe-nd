4677674d686a4164d1637801bb0848f5421f7b75
Bharati Subramanian <bharatisharada@gmail.com>
2022-11-28 17:09:55 +0530

    feat: main home page component

--- starter/src/pages/Home/index.js
+++ starter/src/pages/Home/index.js
@@ -1,7 +1,48 @@
-const Home = () => {
-    return <div>
-        Home
-    </div>
-}
+import { Link } from "react-router-dom";
+import { BookShelf } from "./components/BookShelf";
+import { constants } from "Constants";
+
+const Home = ({
+	shelfBooks = [],
+	onShelfChange,
+	loadingShelf,
+	errorInLoadingShelf,
+}) => {
+	const { SHELVES: shelves = [] } = constants;
+	const getBooksInShelf = (shelf) =>
+		shelfBooks.filter((shelfBook) => shelfBook.shelf === shelf);
+
+	return (
+		<div className="list-books">
+			<div className="list-books-title">
+				<h1>MyReads</h1>
+			</div>
+			{loadingShelf ? (
+				<div className="head">Loading Shelves...</div>
+			) : errorInLoadingShelf ? (
+				<div className="head error">{errorInLoadingShelf}</div>
+			) : (
+				<>
+					<div className="list-books-content">
+						{shelves.map((shelf) => {
+							const books = getBooksInShelf(shelf);
+							return (
+								<BookShelf
+									shelf={shelf}
+									key={shelf}
+									books={books}
+									onShelfChange={onShelfChange}
+								/>
+							);
+						})}
+					</div>
+					<div className="open-search">
+						<Link to="/search">Add a book</Link>
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
 
 export { Home };
